226a957487cfd2f633d40244cb83bfb4155b52bb
RuneNisbeth <35096045+RuneNisbeth@users.noreply.github.com>
2019-03-18 09:32:08 +0100

    Update Load_and_display_picture.py
    manually merging with videoDisplay branch

--- read:show/Load_and_display_picture.py
+++ read:show/Load_and_display_picture.py
@@ -1,11 +1,10 @@
 #import numpy as np
 import cv2
-#from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt
 
-
+img = cv2.imread("/Users/RuneNisbeth/Documents/6semester/fagprojekt/github/PiPyOpenCV/read:show/Nyhavn.jpg")
 #print(img)
 
-# For displaying the video with pyplot. 
 #plt.imshow(img)
 cv2.imshow('image',img)
 cv2.waitKey(0)
